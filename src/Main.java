307ec9794e951c6d730cd654488aa1761fd5b4a0
Egor-Te <103870358+Egor-Te@users.noreply.github.com>
2022-05-26 23:31:04 +0800

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 17391;
+        int bonus = 20;
+        int miles = price / bonus;
+
+        System.out.println("Колличество полученных миль");
+        System.out.println(miles);
+
+    }
+}
